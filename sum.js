e945954857f497170efa0e24188cfaf865619c7a
Alexander Govea <Alexander.g@lakanto.com>
2021-07-07 17:45:51 -0600

    first test on Jest

---
+++ sum.js
@@ -0,0 +1,11 @@
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
